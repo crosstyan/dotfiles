fa9e160e55d0f07ca2ca0e78a0daf8bfb9f68063
crosstyan <cf595833532@gmail.com>
2021-08-06 04:23:53 +0800

    add comments

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -6,8 +6,8 @@ Import-Module oh-my-posh
 
 Set-Alias vscode code
 
+# I have emacs in "C:\root\emax64\bin\"
 Set-Alias "runemacs" "C:\root\emax64\bin\runemacs.exe"
-
 Set-Alias "emacs" "C:\root\emax64\bin\emacs.exe"
 
 Set-PSReadLineOption -EditMode vi
